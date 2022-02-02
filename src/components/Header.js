df73da539f7eea556d5dabafc5f4cfcf9f753618
Mikel-Jon West <dwmyke@gmail.com>
2022-02-01 19:57:18 -0800

    at 1:56:30 in video, finagling nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+    const [showMenu , setShowMenu] = useState('md:hidden')
 
     const menuItems = [
         {
@@ -24,13 +26,33 @@ function Header() {
 
   return (
   <div className='text-white font-robo'>
-     <div className='flex bg-theme justify-between items-center p-4 shadow-lg'>
+     <div className={`flex bg-theme justify-between items-center p-4 shadow-lg ${showMenu!=='md:hidden' && 'flex-col'}`}>
 
-        <div>
+        <div className='flex justify-between w-full'>
             <h1 className='text-3xl font-semibold'>Mikel-Jon (Mike) West</h1>
+            <button 
+            onClick={()=>{
+                if(setShowMenu=='md:hidden'){
+                    setShowMenu('')
+                }
+                else{
+                    setShowMenu('md:hidden')
+                }
+            }}
+            className='hidden md:visible md:flex'>Menu</button>
         </div>
 
-        <div className='flex'>
+        <div className='flex md:hidden'>
+
+{menuItems.map((item)=>{
+    return <li className='list-none mx-5'>
+        <Link to={`/${item.key}`}>{item.title}</Link>
+    </li>
+})}
+
+</div>
+
+        <div className={`md:flex items-start justify-start w-100 flex-col hidden md:visible $(showMenu)`}>
 
             {menuItems.map((item)=>{
                 return <li className='list-none mx-5'>
