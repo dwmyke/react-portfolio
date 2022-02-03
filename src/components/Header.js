341b25399ef30bdccf6c48cb484b59d4124bfafe
Mikel-Jon West <dwmyke@gmail.com>
2022-02-02 19:54:05 -0800

    Fixed navigation, use hidden md:visible instead of lg xl etc

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 function Header() {
 
-    const [showMenu , setShowMenu] = useState('md:hidden')
+    const [showMenu, setShowMenu] = useState("md:hidden");
 
     const menuItems = [
         {
@@ -24,21 +24,24 @@ function Header() {
         },
     ];
 
+    const pathname = window.location.pathname;
+
   return (
   <div className='text-white font-robo'>
-     <div className={`flex bg-theme justify-between items-center p-4 shadow-lg ${showMenu!=='md:hidden' && 'flex-col'}`}>
+     <div className={`flex bg-theme justify-between items-center p-2 shadow-lg ${
+          showMenu == "" && "md:flex-col"
+        } `}>
 
-        <div className='flex justify-between w-full'>
+<div className="flex justify-between items-center w-full">
             <h1 className='text-3xl font-semibold'>Mikel-Jon (Mike) West</h1>
             <button 
-            onClick={()=>{
-                if(setShowMenu=='md:hidden'){
-                    setShowMenu('')
+            onClick={() => {
+                if (showMenu == "md:hidden") {
+                  setShowMenu("");
+                } else {
+                  setShowMenu("md:hidden");
                 }
-                else{
-                    setShowMenu('md:hidden')
-                }
-            }}
+              }}
             className='hidden md:visible md:flex'>Menu</button>
         </div>
 
@@ -52,7 +55,9 @@ function Header() {
 
 </div>
 
-        <div className={`md:flex items-start justify-start w-100 flex-col hidden md:visible $(showMenu)`}>
+<div
+          className={`mt-5 md:flex items-start justify-start w-full flex-col hidden md:visible ${showMenu}`}
+        >
 
             {menuItems.map((item)=>{
                 return <li className='list-none mx-5'>
