ae9a3de2b3a84bfaefe7e516e7ee4827733ddf85
Mikel-Jon West <dwmyke@gmail.com>
2022-02-02 20:33:14 -0800

    Navbar complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa'
 
 function Header() {
 
@@ -34,34 +35,34 @@ function Header() {
 
 <div className="flex justify-between items-center w-full">
             <h1 className='text-3xl font-semibold'>Mikel-Jon (Mike) West</h1>
-            <button 
-            onClick={() => {
+
+            <FaBars onClick={() => {
                 if (showMenu == "md:hidden") {
                   setShowMenu("");
                 } else {
                   setShowMenu("md:hidden");
                 }
               }}
-            className='hidden md:visible md:flex'>Menu</button>
+            className='hidden md:visible md:flex cursor-pointer'/>
         </div>
 
         <div className='flex md:hidden'>
 
 {menuItems.map((item)=>{
-    return <li className='list-none mx-5'>
-        <Link to={`/${item.key}`}>{item.title}</Link>
+    return <li className={`list-none mx-5 p-2 ${item.key==pathname && 'bg-white text-black rounded-md'}`}>
+        <Link to={`${item.key}`}>{item.title}</Link>
     </li>
 })}
 
 </div>
 
 <div
-          className={`mt-5 md:flex items-start justify-start w-full flex-col hidden md:visible ${showMenu}`}
+          className={`mt-3 md:flex items-start justify-start w-full flex-col hidden md:visible ${showMenu}`}
         >
 
             {menuItems.map((item)=>{
-                return <li className='list-none mx-5'>
-                    <Link to={`/${item.key}`}>{item.title}</Link>
+                return <li className={`list-none py-1 ${item.key==pathname && 'bg-white text-black rounded-md px-2'}`}>
+                    <Link to={`${item.key}`}>{item.title}</Link>
                 </li>
             })}
 
