a0f2ba9de6ec53d67732b211e516ae04efeb31f4
Mikel-Jon West <dwmyke@gmail.com>
2022-02-08 10:35:41 +0000

    Projects prototype

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Lottie from "lottie-react";
+import designerAnimation from '../lottie/designer.json';
+import projectsData from '../resources/projects';
 
 function Projects() {
-  return <div>
-      <Layout><h1>Projects</h1></Layout>
-  </div>;
+  return (
+      <Layout>
+        <div className='mt-20'>
+          <div className='h-screen'>
+            <div className='h-3/4'>
+            <Lottie className='h-full' animationData={designerAnimation} loop='true' />
+            </div>
+            <div className='font-semibold font-robo text-center bg-red-500 mx-20 p-10 text-white rounded-tl-full rounded-br-full'>
+            <h1 className='text-6xl'>Many projects</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className='grid mt-20 md:grid-cols-1 lg:grid-cols-2 grid-cols-3 items-center justify-center gap-10 mx-20 md:mx-5 text-robo'>
+          {projectsData.map((project=> {
+            return (
+            <div key={project.id} className='group relative'>
+              <div className='relative p-10 border-2 text-center rounded-3xl h-80 w-full aspect-w-2 group-hover:opacity-75'>
+                <img src={project.image} alt={project.imageAlt} className='w-full h-full rounded-xl object-center object-cover' />
+                </div>
+                
+                  <h1 className='text-3xl font-semibold mt-2'>{project.title}</h1>
+                  <button className='border-2 rounded-2xl mt-2 border-white p-2 hover:bg-green-500'>Demo</button>
+                
+              </div>
+          )}))}
+        </div>
+      </Layout>
+  );
 }
 
 export default Projects;
---
+++ src/resources/projects.js
@@ -0,0 +1,48 @@
+const projectsData = [
+    {
+      title: "Mega Ultra Studios",
+      id: "mega-ultra-studios",
+      image: "./images/mega-ultra-icon.jpeg",
+      imageAlt: "Mega Ultra Studios splash image",
+      description:
+        "Mega Ultra Studios creates short and feature length films, and promotional video packages for performing artists and small business.",
+      link: "/",
+    },
+    {
+      title: "SHEYPIZZA",
+      image: "./pizzas.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "SHEYROOMS",
+      image: "./hotels.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "SHEYJOBS",
+      image: "./jobs.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "SHEYGRAM",
+      image: "./socialmedia.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "SHEYCARS",
+      image: "./cars.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+  ];
+  
+  export default projectsData;
